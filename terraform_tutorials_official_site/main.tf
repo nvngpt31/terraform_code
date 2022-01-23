1ddcdf54cec0adf853c2ea1146bb605e29c9d7ec
Naveen Kumar Gupta <nvngpt31@gmail.com>
2022-01-23 20:03:39 +0530

    Created terraform infra with Docker

---
+++ terraform_tutorials_official_site/main.tf
@@ -0,0 +1,29 @@
+#Create Terraform Infrastructure with Docker
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+	host = "tcp://localhost:2375"
+}
+
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
